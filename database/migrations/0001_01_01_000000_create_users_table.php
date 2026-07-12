018e5dd46d8720c1c42d5ed56198a972beef178e
duan9021021 <i41530267@gmail.com>
2026-07-12 07:57:45 +0700

    melengkapi seeder

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('email');
             $table->string('username')->unique();
             $table->enum('role', ['admin', 'pelanggan'])->default('pelanggan');
             $table->string('password');
--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\KeranjangController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthController::class, 'loginForm']);
+Route::get('/', [AuthController::class, 'loginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-
+Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index']);
+   
     Route::resource('pelanggan', PelangganController::class);
     Route::resource('kategori', KategoriController::class);
     Route::resource('barang', BarangController::class);
@@ -20,7 +20,7 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
 });
 
 Route::middleware(['auth', 'pelanggan'])->prefix('pelanggan')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index']);
+   
     Route::resource('barang', BarangController::class);
     Route::resource('keranjang', KeranjangController::class);
     Route::resource('transaksi', TransaksiController::class);
